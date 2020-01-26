93793300e5e6751e137f6fb1dfa82d507c5b610f
seiyria <kyle@seiyria.com>
2020-01-26 09:36:04 -0600

    add other factions to pick menu

--- src/app/faction-menu/faction-menu.component.ts
+++ src/app/faction-menu/faction-menu.component.ts
@@ -8,6 +8,44 @@ import { PopoverController } from '@ionic/angular';
 })
 export class FactionMenuComponent implements OnInit {
 
+  public factions = [
+    {
+      name: 'Mechanical Marquise',
+      id: 'Marquise',
+      icon: 'marquise'
+    },
+    {
+      name: 'Automated Alliance',
+      id: 'Alliance',
+      icon: 'woodland',
+      disabled: true
+    },
+    {
+      name: 'Clockwork Cult',
+      id: 'Cult',
+      icon: 'cult',
+      disabled: true
+    },
+    {
+      name: 'Electric Eyrie',
+      id: 'Eyrie',
+      icon: 'eyrie',
+      disabled: true
+    },
+    {
+      name: 'Otter Overlords',
+      id: 'Riverfolk',
+      icon: 'riverfolk',
+      disabled: true
+    },
+    {
+      name: 'Robot Raccoon',
+      id: 'Vagabond',
+      icon: 'vagabond',
+      disabled: true
+    }
+  ];
+
   constructor(
     public popoverCtrl: PopoverController
   ) { }
