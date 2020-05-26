cad838bd0140beb2ba8d96dc011591dc35ea947c
Tallon Simmons <tallon.simmons@icloud.com>
2020-05-26 10:49:33 -0400

    Update alliance token interaction
    Set sympathy array defaults; updated token interactions to dynamically add/remove tokens based on action

--- src/app/models/woodland.ts
+++ src/app/models/woodland.ts
@@ -1,23 +1,22 @@
-import { Bot, BotName } from './bot';
+import { Bot, BotName } from "./bot";
 
 export class WoodlandBot extends Bot {
+  public name: BotName = "Woodland";
 
-  public name: BotName = 'Woodland';
-
-  public setupPosition = 'C';
+  public setupPosition = "C";
   public setupRules = [
     `Form a supply of 10 warriors near you.`,
 
     `Collect your 3 bases and place them near you.`,
 
-    `Collect your 10 sympathy tokens and place them near you.`
+    `Collect your 10 sympathy tokens and place them near you.`,
   ];
 
   public difficultyDescriptions = {
     Easy: `
 You only **Organize** if a clearing has four or more Alliance warriors.
     `,
-    Normal: 'Nothing is changed.',
+    Normal: "Nothing is changed.",
     Challenging: `
 You **Organize** if a clearing has two or more Alliance warriors.
     `,
@@ -25,117 +24,137 @@ You **Organize** if a clearing has two or more Alliance warriors.
 You **Organize** if a clearing has two or more Alliance warriors.
 
 At the end of Evening, score one victory point.
-    `
+    `,
   };
 
   public rules = [
     {
-      name: 'Poor Manual Dexterity',
+      name: "Poor Manual Dexterity",
       text: `You have no hand of cards. You cannot discard cards.
       If a human would take a card from you, they draw a card instead.
       If a human would give a card to you, they discard it, and you score **vp:1**.`,
-      isActive: true
+      isActive: true,
     },
     {
-      name: 'Automated Ambush',
-      text: 'In battle as defender with any Alliance warriors, you deal an extra hit.',
-      isActive: true
+      name: "Automated Ambush",
+      text:
+        "In battle as defender with any Alliance warriors, you deal an extra hit.",
+      isActive: true,
     },
     {
-      name: 'Automated Outrage',
+      name: "Automated Outrage",
       text: `Whenever a human removes a sympathy token or moves any warriors into a sympathetic clearing,
       they must discard a matching card. If they cannot, you score **vp:1**.`,
-      isActive: true
+      isActive: true,
     },
     {
-      name: 'Martial Law',
+      name: "Martial Law",
       text: `Whenever you place a sympathy token in a clearing with three or more warriors of an enemy player,
       you score one fewer victory point _(minimum of zero)_.`,
-      isActive: true
+      isActive: true,
     },
     {
-      name: 'Crackdown',
+      name: "Crackdown",
       text: `Whenever a base is removed, remove all sympathy tokens from clearings matching the suit of the base removed.`,
-      isActive: true
+      isActive: true,
     },
     {
-      name: 'Informants',
+      name: "Informants",
       text: `Defenseless sympathy tokens benefit from Automated Ambush.`,
-      canToggle: true
+      canToggle: true,
     },
     {
-      name: 'Popularity',
+      name: "Popularity",
       text: `Enemies do not score victory points for removing sympathy tokens.`,
-      canToggle: true
+      canToggle: true,
     },
     {
-      name: 'Veterans',
+      name: "Veterans",
       text: `
 Gain the Guerilla Warfare ability of the Woodland Alliance. _(In battle as defender, you use the higher roll
   and the attacker uses the lower roll.)_
       `,
-      canToggle: true
+      canToggle: true,
     },
     {
-      name: 'Wildfire',
+      name: "Wildfire",
       text: `At the end of evening, **Spread Sympathy**. Do not score points for placing this sympathy token.`,
-      canToggle: true
+      canToggle: true,
     },
   ];
 
   public customData = {
-    currentSuit: 'bird',
+    currentSuit: "bird",
 
-    sympathy: [],
+    sympathy: [
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+    ],
 
     buildings: {
       fox: false,
       bunny: false,
-      mouse: false
-    }
+      mouse: false,
+    },
   };
 
   public birdsong() {
     const base = [
       `Reveal an order card.`,
-      `Craft order card for **vp:1** if it shows an available item.`
+      `Craft order card for **vp:1** if it shows an available item.`,
     ];
 
-    if (this.customData.currentSuit !== 'bird' && !this.customData.buildings[this.customData.currentSuit]) {
+    if (
+      this.customData.currentSuit !== "bird" &&
+      !this.customData.buildings[this.customData.currentSuit]
+    ) {
       base.push(`
 Remove all enemy pieces from the **card:${this.customData.currentSuit}** sympathetic clearing with the most enemy pieces,
 and place the **card:${this.customData.currentSuit}** base there.
       `);
 
-      const sympathySpread = this.customData.sympathy.slice(0, 5).every(x => x) ? 'once' : 'twice';
+      const sympathySpread = this.customData.sympathy
+        .slice(0, 5)
+        .every((x) => x)
+        ? "once"
+        : "twice";
       base.push(`
 If you did not revolt, **Spread Sympathy** ${sympathySpread}.
       `);
-
     } else {
-      const sympathySpread = this.customData.sympathy.slice(0, 5).every(x => x) ? 'once' : 'twice';
+      const sympathySpread = this.customData.sympathy
+        .slice(0, 5)
+        .every((x) => x)
+        ? "once"
+        : "twice";
       base.push(`
 **Spread Sympathy** ${sympathySpread}.
       `);
-
     }
 
     return base;
   }
 
   public daylight() {
-
     const curSuit = this.customData.currentSuit;
 
     const base = [
-`Place a sympathy token in the **card:${curSuit}** clearing with the fewest enemy warriors adjacent to any sympathetic clearing.
+      `Place a sympathy token in the **card:${curSuit}** clearing with the fewest enemy warriors adjacent to any sympathetic clearing.
 
 (**No Such Clearings**: Instead place a sympathy token on the clearing with the fewest enemy pieces.)
 
 (**Cannot Spread**: If you cannot place a sympathy token, score 5 VP.)`,
     ];
 
-    if (curSuit === 'bird') {
+    if (curSuit === "bird") {
       base.push(`
 Remove all enemy pieces from any sympathetic clearing with the most enemy pieces,
 and place the corresponding base there.
@@ -146,10 +165,13 @@ and place the corresponding base there.
   }
 
   public evening() {
-
-    let organizeVal = 'three';
-    if (this.difficulty === 'Easy') { organizeVal = 'four'; }
-    if (this.difficulty === 'Challenging' || this.difficulty === 'Nightmare') { organizeVal = 'two'; }
+    let organizeVal = "three";
+    if (this.difficulty === "Easy") {
+      organizeVal = "four";
+    }
+    if (this.difficulty === "Challenging" || this.difficulty === "Nightmare") {
+      organizeVal = "two";
+    }
 
     const base = [
       `In each clearing with a base and ${organizeVal} or more Alliance warriors,
@@ -157,14 +179,14 @@ and place the corresponding base there.
 
       `Place a warrior in each clearing with a base.`,
 
-      `Discard the order card.`
+      `Discard the order card.`,
     ];
 
-    if (this.hasTrait('Wildfire')) {
+    if (this.hasTrait("Wildfire")) {
       base.push(`**Spread Sympathy**, but do not score victory points.`);
     }
 
-    if (this.difficulty === 'Nightmare') {
+    if (this.difficulty === "Nightmare") {
       base.push(`Score **vp:1**.`);
     }
 
--- src/app/woodland/woodland.component.ts
+++ src/app/woodland/woodland.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { WoodlandBot } from '../models/woodland';
-import { BotService } from '../bot.service';
-import { RendererService } from '../renderer.service';
+import { Component, OnInit, Input } from "@angular/core";
+import { WoodlandBot } from "../models/woodland";
+import { BotService } from "../bot.service";
+import { RendererService } from "../renderer.service";
 
 @Component({
-  selector: 'app-woodland',
-  templateUrl: './woodland.component.html',
-  styleUrls: ['./woodland.component.scss'],
+  selector: "app-woodland",
+  templateUrl: "./woodland.component.html",
+  styleUrls: ["./woodland.component.scss"],
 })
 export class WoodlandComponent implements OnInit {
-
   @Input() public bot: WoodlandBot;
 
   public sympathyScores = [0, 1, 1, 1, 1, 2, 2, 3, 3, 4];
@@ -17,24 +16,53 @@ export class WoodlandComponent implements OnInit {
   constructor(
     public botService: BotService,
     public rendererService: RendererService
-  ) { }
+  ) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   changeSuit(suit) {
     this.bot.customData.currentSuit = suit;
     this.botService.saveBots();
   }
 
-  toggleSympathy(pos) {
-    this.bot.customData.sympathy[pos] = !this.bot.customData.sympathy[pos];
+  toggleSympathy(sympathyIndex: number) {
+    const sympathyCollection = this.bot.customData.sympathy;
+
+    sympathyCollection[sympathyIndex] = !sympathyCollection[sympathyIndex];
+
+    const tokenIsOnBoard = !sympathyCollection[sympathyIndex];
+    if (tokenIsOnBoard) {
+      this.placeLaterTokensOnBoard(sympathyCollection, sympathyIndex);
+    } else {
+      this.removePriorTokens(sympathyIndex, sympathyCollection);
+    }
     this.botService.saveBots();
+  }
+
+  private removePriorTokens(
+    sympathyIndex: number,
+    sympathyCollection: boolean[]
+  ) {
+    for (let index = 0; index < sympathyIndex; index++) {
+      sympathyCollection[index] = true;
+    }
+  }
+
+  private placeLaterTokensOnBoard(
+    sympathyCollection: boolean[],
+    sympathyIndex: number
+  ) {
+    for (
+      let index = sympathyCollection.length - 1;
+      index > sympathyIndex;
+      index--
+    ) {
+      sympathyCollection[index] = false;
+    }
   }
 
   toggleBuilding(suit) {
     this.bot.customData.buildings[suit] = !this.bot.customData.buildings[suit];
     this.botService.saveBots();
   }
-
 }
