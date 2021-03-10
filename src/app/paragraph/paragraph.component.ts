5854f71111b7654c32ad86f056020acacd706a0e
David Fletcher <david.login@aol.com>
2021-03-10 13:21:30 -0500

    Translate the bot name in Toast component

--- src/app/paragraph/paragraph.component.ts
+++ src/app/paragraph/paragraph.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Bot } from '../models/bot';
+import { BotService } from '../bot.service';
 import { RendererService } from '../renderer.service';
 import { TranslateService } from '@ngx-translate/core';
 import { ToastController } from '@ionic/angular';
@@ -26,13 +27,17 @@ export class ParagraphComponent implements OnInit {
     constructor(
         public rendererService: RendererService,
         public translateService: TranslateService,
+        public botService: BotService,
         public toastController: ToastController
     ) {}
 
     public async updateScore(score: number): Promise<void> {
         this.bot.addVP(score);
+        const botname = this.botService.botMeta[this.bot.name].fullName
+        const translatedBotName = this.translateService.instant('Factions.' + botname)
+
         const toast = await this.toastController.create({
-            message: this.translateService.instant('ToastMessages.UpdateScore', { botname: this.bot.name, addend: score }),
+            message: this.translateService.instant('ToastMessages.UpdateScore', { botname: translatedBotName, addend: score }),
             duration: 3000,
             position: 'bottom',
             buttons: [
